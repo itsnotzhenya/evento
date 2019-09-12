309b783d447d63557987d79f2e028006e76795b2
Dmitriy Klimenko <dmitriyk@webant.ru>
2019-09-12 19:52:00 +0300

    Отчет по пользователям

--- src/components/Users/table/view.vue
+++ src/components/Users/table/view.vue
@@ -3,6 +3,9 @@
     <template slot="crud-title">
       Пользователи
     </template>
+    <template slot="custom-buttons">
+      <el-button @click="getReport" type="primary">Получить отчет по пользователям</el-button>
+    </template>
     <template slot="crud-content">
       <data-table
           module="Users"
@@ -22,6 +25,7 @@ import DataTable from '@/components/DataTable/DataTable'
 import crudMixin from '@/mixins/crudMixin'
 import roles from '@/helpers/roles'
 import usersApi from '@/api/users'
+import quizApi from '@/api/quiz'
 
 export default {
   name: 'UserTable',
@@ -83,6 +87,10 @@ export default {
     },
     async deleteUser (id) {
       await usersApi.delete(id)
+    },
+    async getReport () {
+      const response = await quizApi.index()
+      window.open(`/uploads/${response.data.path}`, '_blank')
     }
   }
 }
